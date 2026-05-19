8ab850d41020d5172f03b48ef3a21c174bf6c7fe
Thierry ImHoTep <tlardoux666@gmail.com>
2026-05-19 20:25:17 +0200

    Controller/Entity/Form bis

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,12 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
+//use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ProductController extends AbstractController
 {
-    #[Route('/produit', name: 'app_gg_produit')]
-    public function produit(): Response
+    public function __construct(private ProductRepository $productRepository, private EntityManagerInterface $entityManager, )
     {
+ 
+    }
+   
+    #[Route('/produit/{id}', name: 'app_gg_product',requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function produit(int $id): Response
+    {
+        //$produit= $this->ProductRepository->find($id);
+
         return $this->render('Produit/produit.html.twig', [
             'controller_name' => 'ProductController',
         ]);
