581965bb608e0428e5d5aba0b6248efd2462280e
Thierry ImHoTep <tlardoux666@gmail.com>
2026-07-06 14:41:25 +0200

    Correction API getProducts

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -6,41 +6,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\SerializerInterface;
 use App\Repository\ProductRepository;
 
 final class APIController extends AbstractController
 {
    #[Route('/api/products',name:'api_product',methods:['GET'])]
-    public function getListProducts(SerializerInterface $serializer, ProductRepository $pr) : JsonResponse
+    public function getListProducts(ProductRepository $pr) : JsonResponse
     {
         // arrive ici ceux qui ont passé avec succès le ApiUserChecker : les comptes actifs avec accès API activé
     
         /** @var \App\entity\User $user */
         $user=$this->getUser();
-
         
-        if ($user->isApiEnabled())
-        { // meme si en principe ceux qui n'ont pas activé API n'arrivent pas ici
-            $productList = $pr->findAll();
-            $jsonProductList = $serializer->serialize($productList,'json',['groups' => 'getProduct']);
-
-            if ($jsonProductList)
-            { // la liste n'est pas vide
-                $errorMessage = "";
-                $codestatus = Response::HTTP_OK;
-               
-                return new JsonResponse([
-                    'productList' => $jsonProductList,
-                    'codestatus' => $codestatus,
-                    'header' => [],
-                    'serialized' => true,
-                ]);
-            }
+        // meme si en principe ceux qui n'ont pas activé API n'arrivent pas ici
+        if(!$user->isApiEnabled())
+        {
+            $errorMessage = 'Accès API non activé';
+            $codestatus = Response::HTTP_FORBIDDEN;            
+            return new JsonResponse(['error'=>$errorMessage],$codestatus,);    
         }
-        // la liste est vide
-        $errorMessage = 'Acces API non active';
-        $codestatus = Response::HTTP_FORBIDDEN;
-        return new JsonResponse(null,$codestatus,['error'=>$errorMessage]);
+
+        $productsList = $pr->findAll();
+
+        $errorMessage = "";
+           
+        return $this->json(
+            ['product' =>$productsList],
+            Response::HTTP_OK,
+            [],
+            ['groups' => 'getProduct'] 
+        );
     }
 }
